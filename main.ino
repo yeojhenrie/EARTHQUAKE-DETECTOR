3e35542733e2ac24570ec12abe13510510705b02
yeojhenrie <65714639+yeojhenrie@users.noreply.github.com>
2023-05-09 13:36:01 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,25 +1,53 @@
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL6juEoPPrY"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "zfWg0W4E0of6DMHRSTkXNCKEb_yaxzoS"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
 #include <LiquidCrystal_I2C.h>
-//#include <BlynkSimpleEsp8266.h>
-//#define BLYNK_PRINT Serial
-//#define BLYNK_TEMPLATE_ID "TMPL6zi4XQoiq"
-//#define BLYNK_TEMPLATE_NAME "New Template"
-//#define BLYNK_AUTH_TOKEN "8l_Cp5cN5DsOqW6s8Oo5JY43n0QTw6y1"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "MONJING";
+char pass[] = "01234567";
+
+BlynkTimer timer;
 LiquidCrystal_I2C lcd(0x27,16,2);
-int a;
-void setup() {
-  //Blynk.begin(BLYNK_AUTH_TOKEN, "PLDTWIFI2.4G", "1014222830");
-  lcd.init();
-  lcd.clear();         
-  lcd.backlight();
-  pinMode(16, INPUT);
-  pinMode(14, OUTPUT);
-  Serial.begin(115200);}
 
-void loop() {
- // Blynk.run();
-  a=digitalRead(16);
-  if(a==1){
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+
+  // Update state
+  Blynk.virtualWrite(V1, value);
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  if(digitalRead(16)==1){
+    Serial.println("Earthquake in the house"); 
     lcd.clear();
     lcd.setCursor(3,0);
     lcd.print("EARTHQUAKE");
@@ -28,8 +56,39 @@ void loop() {
     digitalWrite(14, HIGH);
     delay(5000);
     digitalWrite(14, LOW);
+    Blynk.logEvent("earthquake_detector");
   }else{
     lcd.clear();
   }
-  delay(200);
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();
+  lcd.print("TEST");
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+  pinMode(16, INPUT);
+  pinMode(14, OUTPUT);
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
 }
